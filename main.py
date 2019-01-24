945854556677d26516eee00216fcf6c9851254c4
Cristian Romero <Cristian@bluehubs.com>
2019-01-23 23:25:31 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+"""
+This file main.py is to high light all the attributes
+that could be use to describe a song.
+"""
+
+# Fav Artist #
+Artist = "Drake"
+# Type of music #
+Genre = "Hip hop music"
+# Title #
+Song_Title = "In My Feelings"
+# Duration #
+Duration_In_Seconds = 450
+# Emotion Classification #
+Spectrum_Emotion = "Happy"
+# how easy it is to dance to it #
+Dance_Ability = "High"
+# is it an energetic song? yes #
+Energy = "High"
+# high volume? yes #
+Loudness = "high"
+
+print(Artist)
+print(Genre)
+print(Song_Title)
+print(Duration_In_Seconds)
+print(Spectrum_Emotion)
+print(Dance_Ability)
+print(Energy)
+print(Loudness)
+
