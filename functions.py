13c1091ada1c3c4ec3b98041221d952f1b80b720
Cristian Romero <Cristian@bluehubs.com>
2019-01-24 00:59:44 -0700

    #2: Functions

---
+++ functions.py
@@ -0,0 +1,34 @@
+"""
+#2: Functions
+
+"""
+
+
+def artist():
+    name = "Drake"
+    print(name)
+
+
+def genre():
+    type_genre = "Hip hop music"
+    print(type_genre)
+
+
+def song_title():
+    name_song = "In My Feelings"
+    print(name_song)
+
+
+def artist_confirmation():
+    if artist != "drake":
+        print("Drake confirmed:", True)
+    else:
+        print(False)
+
+
+artist()
+genre()
+song_title()
+artist_confirmation()
+
+
