219fd6891aa131f906f20846f703ec7ae9d7cbed
Cristian R <madgeniusblink@gmail.com>
2019-03-05 23:36:56 -0700

    # Challenge 9 Progress

--- classes/challenge-classes.py
+++ classes/challenge-classes.py
@@ -1,11 +1,12 @@
 class Vehicle:
-    def __init__(self, make = '', model = '', year = 2019, weight = 0):
+    def __init__(self, make = 'NaN', model = 'NaN', year = 2019, weight = 0):
         self.vehicleMake = make
         self.vehicleModel = model
         self.vehicleYear = year 
         self.vehicleWeight = weight
-        self.vechicleNeedMaintence = False
-        self.vechicleTripSinceMaintenance = 0
+        self.vehicleNeedMaintence = False
+        self.vehicleTripSinceMaintenance = 0
+
 
     def setMake(self, make):
         self.vehicleMake = make
@@ -18,15 +19,16 @@ class Vehicle:
 
     def setWeight(self, weight):
         self.vehicleWeight = weight
+
     
     def repair(self):
-        self.vechicleNeedMaintence = False
-        self.vechicleTripSinceMaintenance = 0
+        self.vehicleNeedMaintence = False
+        self.vehicleTripSinceMaintenance = 0
 
     
     
 class Cars(Vehicle):
-    def __init__(self, make="", model="", year=1900, weight=0):
+    def __init__(self, make="NaN", model="NaN", year=1900, weight=0):
         Vehicle.__init__(self, make, model, year, weight)
         self.carIsDriving = False
 
@@ -37,22 +39,24 @@ class Cars(Vehicle):
     def stop(self):
         self.carIsDriving = False
         if self.carIsDriving is True:
-            self.vechicleTripSinceMaintenance += 1
-        elif self.vechicleTripSinceMaintenance > 100:
-            self.vechicleNeedMaintence = True
+            self.vehicleTripSinceMaintenance += 1
+        elif self.vehicleTripSinceMaintenance > 100:
+            self.vehicleNeedMaintence = True
 
-
+    def __str__(self):
+        return "Car Info: " + self.vehicleMake + "\n" 
 
 bmw = Cars(make='BMW', model='i8', year=2019, weight=4000)
 tesla = Cars(make='tesla', model='mode 2', year=2019, weight=4000)
 toyota = Cars(make='toyota', model='prius', year=2019, weight=4000)
     
-def drive(car, times):
-    for x in range(times):
-        car.drive()
-        car.stop()
 
 print(bmw)
-drive(tesla, 100)
-print(tesla)
+print(bmw.vehicleMake)
+print(bmw.vehicleNeedMaintence)
+bmw.drive()
+bmw.stop()
+bmw.repair()
+print(bmw.vehicleNeedMaintence)
+print(tesla.vehicleMake)
 
